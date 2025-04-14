4bb29541a4914e9529ca0f81dae0aa0667f6b4ec
Issamdev_sol <mindseteur.3@gmail.com>
2025-04-14 02:52:08 +0200

    Update BuyMeACoffee.sol

--- BuyMeACoffee.sol
+++ BuyMeACoffee.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 contract BuyMeACoffee {
-    // Structure d’un "tip"
+    
     struct Memo {
         address from;
         uint256 timestamp;
@@ -10,20 +10,20 @@ contract BuyMeACoffee {
         string message;
     }
 
-    // Adresse du propriétaire du contrat
+    
     address payable public owner;
 
-    // Liste des tips
+    
     Memo[] public memos;
 
-    // Events
+    
     event NewMemo(address indexed from, uint256 timestamp, string name, string message);
 
     constructor() {
         owner = payable(msg.sender);
     }
 
-    // Envoyer un tip avec un message
+    
     function buyCoffee(string memory _name, string memory _message) public payable {
         require(msg.value > 0, "Can't buy coffee for free!");
 
@@ -32,12 +32,12 @@ contract BuyMeACoffee {
         emit NewMemo(msg.sender, block.timestamp, _name, _message);
     }
 
-    // Lire tous les messages
+    
     function getMemos() public view returns (Memo[] memory) {
         return memos;
     }
 
-    // Retirer les fonds
+   
     function withdrawTips() public {
         require(msg.sender == owner, "Only the owner can withdraw");
         require(address(this).balance > 0, "No tips to withdraw");
